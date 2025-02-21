dc07ff89d4924addc17ac50782aa73d6f226f6ec
Carlos Faustino <chfcchfc96@gmail.com>
2025-02-21 16:31:35 -0300

    feat: move files

--- web/src/app/invite/[subscriberId]/page.tsx
+++ web/src/app/invite/[subscriberId]/page.tsx
@@ -4,8 +4,16 @@ import { InviteLinkInput } from "./invite-link-input"
 import { Ranking } from "./ranking"
 import { Stats } from "./stats"
 
-export default function InvitePage() {
-  const inviteLink = "http://localhost:3000/invite/123"
+interface InvitePageProps {
+  params: Promise<{
+    subscriberId: string
+  }>
+}
+
+export default async function InvitePage(props: InvitePageProps) {
+  const { subscriberId } = await props.params
+
+  const inviteLink = `http://localhost:3333/invites/${subscriberId}`
 
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
